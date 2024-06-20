e6198c0f1d64c0fa561faa157100d9ba325f9ce9
ggilggilmonster <167064562+ggilggilmonster@users.noreply.github.com>
2024-06-20 10:49:24 +0900

    Add files via upload

---
+++ programmers/나누어 떨어지는 숫자 배열.kt
@@ -0,0 +1,16 @@
+package com.example.programmers
+
+class Solution {
+    fun solution(arr: IntArray, divisor: Int): IntArray {
+        var answer = intArrayOf()
+        for(i in arr) {
+            if(i % divisor == 0) {
+                answer += i
+            }
+        }
+        if(answer.isEmpty()) {
+            answer += -1
+        }
+        return answer.sorted().toIntArray()
+    }
+}
